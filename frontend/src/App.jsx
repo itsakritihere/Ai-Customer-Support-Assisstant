d3c8ae3054b077da3f2a8ce52ef5cfdb78e8d6da
Akriti Chauhan <chauhanakriti66@gmail.com>
2026-08-13 11:33:44 +0530

    Update FastAPI backend and r

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://ai-customer-support-assisstant.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
